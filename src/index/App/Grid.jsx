a43c8c1463c91c182b7916e60546a85c9328cab9
clalexni <cl.alexni@gmail.com>
2022-10-08 21:27:53 -0500

    make Calculator column scrollable

--- src/index/App/Grid.jsx
+++ src/index/App/Grid.jsx
@@ -5,6 +5,8 @@ import Calculator from "./Grid/Calculator";
 import Map from "./Grid/Map";
 import useLocalStorage from "./shared/useLocalStorage";
 
+import "./Grid/Grid.css";
+
 function Grid() {
 	const [points, setPoints] = useLocalStorage("points", []);
 
@@ -13,7 +15,7 @@ function Grid() {
 			<Col xs="auto">
 				<Map points={points} setPoints={setPoints} />
 			</Col>
-			<Col>
+			<Col className="scrollable">
 				<h2>Map Distance Calculator</h2>
 				<hr />
 				<Calculator points={points} setPoints={setPoints} />
